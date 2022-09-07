c6312dcac9e5cdb887adf28fea3f6966e686615b
tbenko <t.benko@vig-re.com>
2022-09-07 22:38:37 +0200

    prvni coding

---
+++ textovy_analyzator.py
@@ -0,0 +1,20 @@
+"""
+projekt_1.py: první projekt do Engeto Online Python Akademie
+author: Tomas Benko
+email: tomas.benko.tb@gmail.com
+discord: Tomas B.#3206
+"""
+
+import pandas as pd
+import numpy as np
+import os
+import pprint
+
+uzivatele = {"bob":"123","ann":"pass123","mike":"password123","liz":"pass123"}
+
+jmeno = input("Prihlasovaci jmeno: ")
+heslo = input("Heslo: ")
+
+if jmeno.isin()
+
+pozdrav = f"""Dobry den, {jmeno}, prosim, nyni muzes zacit analyzovat texty.
